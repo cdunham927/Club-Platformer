fc3efadeb3959749ea96aae29d878d0e788d5505
cdunham927 <cdunham927@gmail.com>
2018-08-21 14:38:23 -0700

    Fixed it

---
+++ Club-Platformer/Club-Platformer-master/RunAndGun/Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageHandler : MonoBehaviour
+{
+
+    public int health = 1;
+    public float invulnPeriod = 0;
+    float invulnTimer = 0;
+    int correctLayer;
+
+    void Start()
+    {
+        correctLayer = gameObject.layer;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        health--;
+        invulnTimer = invulnPeriod;
+        gameObject.layer = 10;
+    }
+    void Update()
+    {
+        invulnTimer -= Time.deltaTime;
+        if (invulnTimer <= 0)
+        {
+            gameObject.layer = correctLayer;
+        }
+        if (health <= 0)
+        {
+
+            Die();
+
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
